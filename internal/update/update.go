afb3829c1778d6764414ff81358edd98446000e4
mudaaaa <jonathanmnargi@gmail.com>
2025-11-19 20:28:43 -0500

    fix: version shit

--- internal/update/update.go
+++ internal/update/update.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	githubApiUrl = "https://api.github.com/repos/charmbracelet/crush/releases/latest"
+	githubApiUrl = "https://api.github.com/repos/mudaaaa/crushplus/releases/latest"
 	userAgent    = "crush/1.0"
 )
 
